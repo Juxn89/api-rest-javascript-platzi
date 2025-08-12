2ebad14190aee1c0badff3f6927643d37504b70b
Juan Gómez <gb.jc@outlook.com>
2025-08-12 13:14:18 -0400

    Added navigation between pages.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 const BASE_URL = 'https://api.themoviedb.org/3/'
 const BASE_URL_300 = 'https://image.tmdb.org/t/p/w300'
 
+const api = axios.create({
+	baseURL: 'https://api.themoviedb.org/3/',
+	headers: {
+		'Authorization': API_KEY,
+		'Content-Type': 'application/json;charset=utf-8'
+	}
+})
+
 async function getTrendingMoviesPreview() {
-	const response = await fetch(`${BASE_URL}trending/movie/day`, { headers: { 'Authorization': API_KEY } })
-	const data = await response.json()
+	const { data } = await api.get('trending/movie/day')
 	const movies = await data.results
 
 	movies.forEach(movie => {
 		const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
 
-		const  movieContainer = document.createElement('div')
+		const movieContainer = document.createElement('div')
 		movieContainer.classList.add('movie-container')
 
 		const movieImg = document.createElement('img')
@@ -20,7 +27,26 @@ async function getTrendingMoviesPreview() {
 		movieContainer.appendChild(movieImg)
 		trendingPreviewMoviesContainer.appendChild(movieContainer)
 	})
-
 }
 
-getTrendingMoviesPreview()
+async function getCategoriesPreview() {
+	const { data } = await api.get('genre/movie/list')
+	const categories = await data.genres
+
+	categories.forEach(category => {
+		const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+
+		const categoryContainer = document.createElement('div')
+		categoryContainer.classList.add('category-container')
+
+		const categoryTitle = document.createElement('h3')
+		categoryTitle.classList.add('category-title')
+		categoryTitle.setAttribute('id', `id${ category.id }`)
+
+		const categoryTitleText = document.createTextNode(category.name)
+
+		categoryTitle.appendChild(categoryTitleText)
+		categoryContainer.appendChild(categoryTitle)
+		previewCategoriesContainer.appendChild(categoryContainer)
+	})
+}
